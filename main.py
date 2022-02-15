a7a18b75099118e905331e04394de506df7b98d4
rigobert9 <francois.gallois.pro@gmail.com>
2022-02-15 21:48:32 +0100

    Ajout d'un main.py Dedans sont spécifiés un certain nombre de choses sur la structure

---
+++ main.py
@@ -0,0 +1,20 @@
+# Importations des moteurs de jeu
+# Importation du parser de script (transforme le script en arbre)
+# ^ Ce précédent fichier se charge d'importer la structure d'arbre binaire
+
+# Une fois commencé, le jeu évolue à l'aide d'une fonction récursive : un choix
+# ou un passage de texte appelle la fonction de lecture du moteur vers le texte
+# suivant
+
+# Structure : Une fonction init/main, qui crée la fenêtre et appelle le jeu
+# Lorsque le jeu se termine, il est appelé à nouveau si on rejoue
+
+# La fonction de jeu est récursive, et fait les appels au moteur de jeu
+
+# En fonction des implications, la fenêtre de jeu pourra être une classe, un
+# module ou simplement des fonctions
+
+# Ce fichier, comme toujours, est le fichier principal qui appelle tous le
+# autres composants :
+# Le moins possible doit être défini ici, et il annonce simplement la façon dont
+# les composants intéragissent
