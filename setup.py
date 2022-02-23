f14635cd9c2a2c109bc38697a857f591b3d50776
user <francois.gallois.pro@gmail.com>
2022-02-23 19:23:09 +0100

    Ajout du setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# Fichier de configuration permettant de transformer le projet en module
+
+from setuptools import setup
+
+setup(name='N-RPG',
+        version='0.42',
+        description='Module nécessaire au fonctionnement du N-RPG',
+        packages=['N-RPG']
+        )
