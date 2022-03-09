b011dc731d989165652dc2a904fc3e7ea3340f21
Mcpineapple <tristan.bincaz@gmail.com>
2022-03-09 17:37:45 +0100

    wrappers réparés et ajout de documentation

--- nrpg/arbre.py
+++ nrpg/arbre.py
@@ -2,8 +2,21 @@
 # arbre binaire pour représenter l'histoire. La classe sera utilisée par le
 # parser de script pour y mettre l'histoire.
 class Arbre:
+    u"""
+    Arbre binaire simple permettant de contenir le script et ses embranchements.
+    Les noeuds contiendront du texte correspondant à l'histoire.
+    Les arêtes de l'arbre contiennent du texte qui décriront les embranchements.
+    """
 
     def __init__(self, texte : str):
+        u"""
+        Constructeur de la classe Arbre.
+        Préconditions :
+            Paramètres : 
+                texte : str, correspond au script de la racine
+        Postconditions :
+            Création d'une classe Arbre qui contiendra le script utilisable par le Moteur.
+        """
 
         assert isinstance(texte, str), u"L'arbre doit contenir des chaînes de caractères."
 
@@ -15,29 +28,61 @@ class Arbre:
         self.arete_gauche = u""
         self.arete_droit = u""
 
-    @property
-    def inserer_fils_gauche(self, texte : str):
+    @property.setter
+    def fils_gauche(self, texte : str) -> None:
+        u"""
+        Permet de creer un fils gauche au Noeud.
+        Préconditions:
+            Paramètres:
+                texte: str, correspond au script du noeud créé
+        Postconditions:
+            Creer un fils gauche, un arbre.
+        """
 
         assert isinstance(texte, str), u"L'arbre doit contenir des chaînes de caractères."
 
         self.fils_gauche = Arbre(texte)
 
-    @property
-    def inserer_arete_gauche(self, texte : str):
+    @property.setter
+    def arete_gauche(self, texte : str):
+        u"""
+        Change le texte de l'arête gauche.
+        Préconditions:
+            Paramètres:
+                texte : str, sera le texte de l'arête
+        Postconditions:
+            Le texte de l'arête gauche devient le texte entré.
+        """
 
         assert isinstance(texte, str), u"L'arbre doit contenir des chaînes de caractères."
 
         self.arete_gauche = texte
 
-    @property
-    def inserer_fils_droit(self, texte : str):
+    @property.setter
+    def fils_droit(self, texte : str):
+        u"""
+        Permet de creer un fils droit au Noeud.
+        Préconditions:
+            Paramètres:
+                texte: str, correspond au script du noeud créé
+        Postconditions:
+            Creer un fils droit, un arbre.
+        """
 
         assert isinstance(texte, str), u"L'arbre doit contenir des chaînes de caractères."
 
         self.fils_droit = Arbre(texte)
     
-    @property
-    def inserer_arete_droit(self, texte : str):
+    @property.setter
+    def arete_droit(self, texte : str):
+        u"""
+        Change le texte de l'arête droite.
+        Préconditions:
+            Paramètres:
+                texte : str, sera le texte de l'arête
+        Postconditions:
+            Le texte de l'arête droite devient le texte entré.
+        """
 
         assert isinstance(texte, str), u"L'arbre doit contenir des chaînes de caractères."
         
@@ -45,21 +90,48 @@ class Arbre:
 
 
     @property
-    def recuperer_fils_gauche(self):
+    def fils_gauche(self):
+        u"""
+        Renvoit le fils gauche.
+        Préconditions:
+            Aucune
+        Postconditions:
+            self.fils_gauche : None ou un Arbre
+        """
         
         return self.fils_gauche
 
     @property
-    def recuperer_fils_droit(self):
-
+    def fils_droit(self):
+        u"""
+        Renvoit le fils droit.
+        Préconditions:
+            Aucune
+        Postconditions:
+            self.fils_droit : None ou un Arbre
+        """
         return self.fils_droit
 
     @property
-    def lire_arete_gauche(self):
+    def arete_gauche(self):
+        u"""
+        Renvoit le texte de l'arête gauche.
+        Préconditions:
+            Aucune
+        Postconditions:
+            self.arete_gauche : str, texte de l'arête gauche
+        """
 
         return self.arete_gauche
 
     @property
-    def lire_arete_droit(self):
+    def arete_droit(self):
+        u"""
+        Renvoit le texte de l'arête droite.
+        Préconditions:
+            Aucune
+        Postconditions:
+            self.arete_droit : str, texte de l'arête droit  
+        """
 
         return self.arete_droit
