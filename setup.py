215830a5f5addab5ec3bbe09d2e76099b717f64e
user <francois.gallois.pro@gmail.com>
2022-03-24 15:53:04 +0100

    Mise à jour du setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 # Fichier de configuration permettant de transformer le projet en module
 
 from setuptools import setup
+import os
+
+thelibFolder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+install_requires = []
+if os.path.isfile(thelibFolder):
+    with open(thelibFolder) as f:
+        install_requires = f.read().splitlines()
 
 setup(name='nrpg',
         version='0.42',
         description='Module nécessaire au fonctionnement du N-RPG',
-        packages=['nrpg']
+        packages=['nrpg'],
+        install_requires=install_requires
         )
